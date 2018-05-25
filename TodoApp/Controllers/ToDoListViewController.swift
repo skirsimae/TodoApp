570f0fbf945795756813b10028d2efb6850cf020
Silva Kirsimae <silva.kirsimae-cic.netherlands@ibm.com>
2018-05-25 14:03:22 +0200

    Persisting data using Codable protocol and Plist.

--- TodoApp/Controllers/ToDoListViewController.swift
+++ TodoApp/Controllers/ToDoListViewController.swift
@@ -11,28 +11,14 @@ import UIKit
 class ToDoListViewController: UITableViewController {
     
     var itemArray = [Item]()
-    
-    let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let newItem = Item()
-        newItem.title = "Find Eggos"
-        itemArray.append(newItem)
+        print(dataFilePath)
         
-        let newItem2 = Item()
-        newItem2.title = "Find Silva"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Find Robert"
-        itemArray.append(newItem3)
-        
-        //retrive persistent data
-        if let items = defaults.array(forKey: "ToDoListArray") as? [Item] {
-            itemArray = items
-        }
+        loadItems()
     }
     
     //MARK - TableView Datasource Methods
@@ -67,10 +53,8 @@ class ToDoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //add check mark, remove on second tap
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-        
-        //forces the tableView to call its data sources methods again to reload data
-        tableView.reloadData()
-        
+        saveItems()
+
         //deselect the row to get rid of the grey background of the cell
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -85,10 +69,8 @@ class ToDoListViewController: UITableViewController {
             let newItem = Item()
             newItem.title = textField.text!
             self.itemArray.append(newItem)
-            
-            //save the itemArray to defaults
-            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
-            self.tableView.reloadData()
+        
+            self.saveItems()
         }
         
         alert.addTextField { (alertTextField) in
@@ -98,6 +80,34 @@ class ToDoListViewController: UITableViewController {
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+    }
+    
+    //MARK - Model Manipulation Methods
+    
+    func saveItems() {
+        //save the itemArray to defaults
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        //forces the tableView to call its data sources methods again to reload data
+        self.tableView.reloadData()
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print(error)
+            }
+        }
     }
 }
 
